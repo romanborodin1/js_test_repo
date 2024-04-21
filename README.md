255f239ad2b247d9296b258ebc591c42609155ea
Roman Borodin <borodinrs1@gmail.com>
2024-04-21 15:25:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS_TEST_REPO
+
---
+++ js/main.js
@@ -0,0 +1,9 @@
+var seconds = 60;
+var minutes = 60;
+// Comment
+/* Multi 
+  string
+  comment
+*/
+var secondsInHours = seconds*minutes;
+console.log("Секунд в часе: " + secondsInHours);
